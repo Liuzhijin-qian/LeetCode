3402fb72ae6114bc30d5e529aaa19b999ebf30c7
200901114-Liu Wei <1473120002@qq.com>
2022-05-24 21:13:19 +0800

    New

--- 34.在排序数组中查找元素的第一个和最后一个位置.java
+++ 34.在排序数组中查找元素的第一个和最后一个位置.java
@@ -15,15 +15,47 @@ class Solution {
     _left = left_bound(nums, target);
     _right = right_bound(nums, target);
     array[0] = _left;
-    array[1] = _left;
+    array[1] = _right;
     return array;
   }
-  int left_bound(int[] nums,target){
-      int left = 0;
-      int right = nus.length - 1;
-      while(left <= right){
-        int mid = left + (right-left)/2;          
+
+  int left_bound(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        right = mid - 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
       }
+    }
+    //检查越界情况
+    if (left >= nums.length || nums[left] != target) {
+      return -1;
+    }
+    return left;
+  }
+
+  int right_bound(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        left = mid + 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      }
+    }
+    if (right < 0 || nums[right] != target) {
+      return -1;
+    }
+    return right;
   }
 }
 // @lc code=end
